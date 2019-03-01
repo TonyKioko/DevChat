e6b5441e26999c23442caf0989c43376c6b902da
TonyKioko <tonykioko384@gmail.com>
2019-03-01 21:48:10 +0300

    user registration form added using semantic ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -8,7 +7,7 @@ import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 
-
+import 'semantic-ui-css/semantic.min.css'
 
 const Root = () =>(
     <Router>
