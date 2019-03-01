a664a69195a22a4030baf6fd0b77b76c2160efd9
TonyKioko <tonykioko384@gmail.com>
2019-03-01 20:08:07 +0300

    organized project & created routes

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+
+
+
+const Root = () =>(
+    <Router>
+<Switch>
+
+    <Route exact path="/" component={App}> </Route>
+    <Route path="/login" component={Login}></Route>
+    <Route path="/register" component={Register}></Route>
+
+
+
+</Switch>
+
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
