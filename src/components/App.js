9459d53e577ccae799dcb45a4c6c28a3faa895bc
TonyKioko <tonykioko384@gmail.com>
2019-03-03 17:54:29 +0300

    scaffold app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
+import ColorPanel from '../ColorPanel/ColorPanel';
+import SidePanel from '../SidePanel/SidePanel';
+import Messages from '../Messages/Messages';
+import MetaPanel from '../MetaPanel/MetaPanel';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
-      </div>
+     <Grid columns="equal" className="app" style={{background:'#eee'}}>
+
+      <ColorPanel />
+      <SidePanel />
+<Grid.Column style={{marginLeft:320}}>
+
+<Messages />
+</Grid.Column>
+
+      
+<Grid.Column width={4}>
+
+<MetaPanel />
+
+</Grid.Column>
+
+
+
+     </Grid>
     );
   }
 }
